04ae42687dcb46441ff5e34db434800298121830
ahyaemon <iamahyaemon@gmail.com>
2019-04-11 21:47:17 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ggez のサンプル
+
+  ggez のテンプレ的な
+
+
+## ggez
+
+  https://github.com/ggez
+
+
+## やっていること
+
+  - 画像の描画
+
+  - キー入力
+    - からの移動
+
+  - BGM, SE 再生
+
+
+## 効果音
+
+  - [効果音ラボ 様](https://soundeffect-lab.info/sound/anime/)
---
+++ src/main.rs
@@ -0,0 +1,129 @@
+extern crate cgmath;
+extern crate ggez;
+
+use ggez::event;
+use ggez::event::KeyCode;
+use ggez::event::KeyMods;
+
+use ggez::graphics;
+use ggez::{Context, GameResult};
+use std::env;
+use std::path;
+
+use ggez::audio;
+use ggez::audio::SoundSource;
+
+struct MainState {
+    image: graphics::Image,
+
+    bgm: audio::Source,
+    se: audio::Source,
+
+    x: f32,
+    y: f32,
+    speed: f32,
+
+    up_pressing: bool,
+    right_pressing: bool,
+    down_pressing: bool,
+    left_pressing: bool,
+    space_pressed: bool,
+}
+
+impl MainState {
+
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let image = graphics::Image::new(ctx, "/stand01.png")?;
+        let bgm = audio::Source::new(ctx, "/juvenile.mp3")?;
+        let se = audio::Source::new(ctx, "/hyoushigi1.mp3")?;
+        Ok(MainState{
+            image,
+            bgm,
+            se,
+            x: 100.0,
+            y: 100.0,
+            speed: 3.0,
+            up_pressing: false,
+            right_pressing: false,
+            down_pressing: false,
+            left_pressing: false,
+            space_pressed: false,
+        })
+    }
+
+}
+
+impl event::EventHandler for MainState {
+
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        // 移動制御
+        if self.up_pressing { self.y -= self.speed; }
+        if self.right_pressing { self.x += self.speed; }
+        if self.down_pressing { self.y += self.speed; }
+        if self.left_pressing { self.x -= self.speed; }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, graphics::BLACK);
+
+        // プレイヤーの描画
+        let dest = cgmath::Point2::new(self.x, self.y);
+        graphics::draw(ctx, &self.image, (dest,))?;
+
+        graphics::present(ctx)?;
+
+        Ok(())
+    }
+
+    fn key_down_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods, _repeat: bool) {
+        match keycode {
+            KeyCode::A => { self.left_pressing = true; }
+            KeyCode::S => { self.down_pressing = true; }
+            KeyCode::D => { self.right_pressing = true; }
+            KeyCode::W => { self.up_pressing = true; }
+            // 最初の key_down の時のみ効果音を鳴らす
+            KeyCode::Space => {
+                if self.space_pressed == false {
+                    let _  = self.se.play();
+                }
+                self.space_pressed = true
+            }
+            _ => {}
+        }
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods) {
+        match keycode {
+            KeyCode::A => { self.left_pressing = false; }
+            KeyCode::S => { self.down_pressing = false; }
+            KeyCode::D => { self.right_pressing = false; }
+            KeyCode::W => { self.up_pressing = false; }
+            KeyCode::Space => { self.space_pressed = false; }
+            _ => {}
+        }
+    }
+
+}
+
+pub fn main() -> GameResult {
+    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        path
+    } else {
+        path::PathBuf::from("./resources")
+    };
+    let cb = ggez::ContextBuilder::new("drawing", "ggez").add_resource_path(resource_dir);
+    let (ctx, events_loop) = &mut cb.build()?;
+    println!("{}", graphics::renderer_info(ctx)?);
+    let state = &mut MainState::new(ctx).unwrap();
+
+    // bgm をリピートで流す
+    &state.bgm.set_repeat(true);
+    &state.bgm.play_detached();
+
+    // ゲーム開始
+    event::run(ctx, events_loop, state)
+}
